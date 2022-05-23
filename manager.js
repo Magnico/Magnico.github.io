383e0aa3a038566d8e9d59560f6837fa71f0ca99
Magnico <ejmrebolledo@gmail.com>
2022-05-23 13:58:31 -0500

    Algunas cosas

--- manager.js
+++ manager.js
@@ -418,3 +418,12 @@ function drawMarco(){
   }
   container.appendChild(table)
 }
+
+function imprimirBitacora(){
+  var ficha = document.getElementById('Comments');
+  var ventimp = window.open(' ', 'popimpr');
+  ventimp.document.write( ficha.innerHTML );
+  ventimp.document.close();
+  ventimp.print( );
+  ventimp.close();
+}
